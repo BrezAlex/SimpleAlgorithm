764ff462d722841caa01633666d0636f154d72fe
Alexandr Brezin <sasha.brezin7262@gmail.com>
2020-05-24 19:22:39 +0300

    Добавлены финальные изменениея, оптимизация кода.

--- Program.cs
+++ Program.cs
@@ -11,56 +11,48 @@ namespace SimpleAlgorithm
             var min = m[0];
             for (var i = 1; i < m.Length; i++)
             {
-                if (m[i] < min)
-                {
-                    min = m[i];
-                }
+                if (m[i] >= min) continue;
+                min = m[i];
             }
             Console.WriteLine("Min={0}", min);
             var max = m[0];
             for (var i = 1; i < m.Length; i++)
             {
-                if (m[i] > max)
-                {
-                    max = m[i];
-                }
+                if (m[i] <= max) continue;
+                max = m[i];
             }
             Console.WriteLine("Max={0}", max);
             for (var i = 0; i < m.Length - 1; i++)
             {
                 for (var j = i + 1; j < m.Length; j++)
                 {
-                    if (m[i] > m[j])
-                    {
-                        var option = m[i];
-                        m[i] = m[j];
-                        m[j] = option;
-                    }
+                    if (m[i] <= m[j]) continue;
+                    var option = m[i];
+                    m[i] = m[j];
+                    m[j] = option;
                 }
             }
             Console.WriteLine("Сортировка по возрастанию:");
-            for (int i = 0; i < m.Length; i++)
+            foreach (var t in m)
             {
-                Console.WriteLine(m[i]);
+                Console.WriteLine(t);
             }
 
-            for (int i = 0; i < m.Length - 1; i++)
+            for (var i = m.Length - 1 - 1; i >= 0; i--)
             {
-                for (int j = i + 1; j < m.Length; j++)
+                for (var j = m.Length - 1; j > i; j--)
                 {
-                    if (m[i] < m[j])
-                    {
-                        var option1 = m[i];
-                        m[i] = m[j];
-                        m[j] = option1;
-                    }
+                    if (m[i] >= m[j]) continue;
+                    var option1 = m[i];
+                    m[i] = m[j];
+                    m[j] = option1;
                 }
             }
             Console.WriteLine("Сортировка по убыванию:");
 
-            for (int i = 0; i < m.Length; i++)
+            foreach (var t in m)
             {
-                Console.WriteLine(m[i]);
+                Console.WriteLine(t);
             }
             Console.ReadKey();
 
